9d21801ec9d8e34a5b102ba408f5699b6448060d
varaprasadh <varaprasadh.a@gmail.com>
2019-10-27 15:06:55 +0530

    initalisation setup

---
+++ app/containers/Root.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Root extends Component {
+    render() {
+        return (
+            <div>
+                test
+            </div>
+        )
+    }
+}
+
+export default Root
